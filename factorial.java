4ee5c9690ff1bec1c95551b610d8616535e35d25
Student <yaseensha01@gmail.com>
2021-10-12 15:10:52 +0530

    SORRY

--- factorial.java
+++ factorial.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class factorial {
-    public static void main(String[] args) {
-        
-    }
+  public int getFactorial(int userInput) {
+    int i, factorial = 1;
+    for (i = 1; i <= userInput; i++)
+      factorial *= i;
+    return factorial;
+  }
+
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    System.out.print("enter a number : ");
+    int factorial, userValue = myScanner.nextInt();
+    FactorialOfNumber fact = new FactorialOfNumber();
+    factorial = fact.getFactorial(userValue);
+    System.out.println(factorial);
+
+    myScanner.close();
+  }
+
 }
